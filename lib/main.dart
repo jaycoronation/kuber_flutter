966f87816ad5597781bf78f6dd3d5de2c44f5493
jaycoronation <jay.m@coronation.in>
2023-08-02 16:54:52 +0530

    Version 2 From Mac Mini

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:audioplayers/audioplayers.dart';
-import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -20,11 +19,11 @@ import 'package:firebase_core/firebase_core.dart';
   await Firebase.initializeApp();
 
    FlutterError.onError = (errorDetails) {
-     FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
+     //FirebaseCrashlytics.instance.recordFlutterFatalError(errorDetails);
    };
    // Pass all uncaught asynchronous errors that aren't handled by the Flutter framework to Crashlytics
    PlatformDispatcher.instance.onError = (error, stack) {
-     FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+     //FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
      return true;
    };
 
